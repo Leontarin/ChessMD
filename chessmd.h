7b38ac9b6b61b880186a3aa852ec0b525b998e10
Leo Feldman <leolight214@gmail.com>
2022-11-14 09:25:03 +0200

    Commands partially finished, requires logic and game

--- chessmd.h
+++ chessmd.h
@@ -9,17 +9,23 @@ enum class PTYPE { NONE, PAWN, ROOK, KNIGHT, BISHOP, KING, QUEEN, last }; //PIEC
 class ChessMD {
 private:
 	bool _running;
-	short int turn;
+	bool _game;
+	PCOL turn;
+	PCOL checked;
+	PCOL winner;
 	typedef struct Piece {
 		PTYPE type;
 		PCOL color;
 	} Piece;
 	typedef struct Cell {
 		struct Piece p;
-		bool sel;
+		bool** sel = new bool * [8]; //Cell selection
 		bool mov;
 	} Cell;
 	Cell** board = new Cell * [8];
+	bool** boardSel = new bool * [8]; //board current piece selection
+	bool** whiteChecked = new bool* [8]; //white checked territory
+	bool** blackChecked = new bool* [8]; //black checked territory
 	void initBoard(Cell**);
 	void initGame(Cell**);
 	void initBoardPlacement(Cell**);
@@ -27,6 +33,9 @@ private:
 	std::string lastError;
 	Cell* cSel;
 	Cell* stringToCell(std::string);
+	bool parseEvent(std::string);
+	bool isCellValid(Cell*);
+	PCOL updateSelection();
 
 public:
 	ChessMD();
